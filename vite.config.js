1b70204d8600ad04a764c263bc65a68260dc67e0
jeyreichel <jeyreichel@gmail.com>
2024-06-14 02:38:13 +0800

    fix: dd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/3D-art-gallery',
   build: {
     rollupOptions: {
-      input: '/main.js',
+      input: "/main.js",
     },
   },
 });
